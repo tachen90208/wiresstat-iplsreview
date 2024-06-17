59edbc59fa63f45bfd730537552930db394069b9
Ting An <stu90208@gmail.com>
2024-06-17 18:13:29 +0800

    Update CV_merra2_ISIMPLS.ipynb, ISIMPLS.py

--- Code/ISIMPLS.py
+++ Code/ISIMPLS.py
@@ -49,6 +49,7 @@ class ISIMPLS(BaseEstimator):
         self.C = None
         self.S = None
 
+
     def _PLS1_ProjMat(self,S):
         W_rotations = np.zeros((self.n_components,self.n_features))
         V = np.zeros((self.n_components,self.n_features))
@@ -67,26 +68,37 @@ class ISIMPLS(BaseEstimator):
 
     def fit(self,X,y):
         X = check_array(X, dtype=FLOAT_DTYPES, copy=self.copy)
-        # y = check_array(y, dtype=FLOAT_DTYPES, copy=self.copy)
+        y = check_array(y.reshape(-1,1), dtype=FLOAT_DTYPES, copy=self.copy)
+
         if self.n == 0:
             self.n_features = X.shape[1]
             self.C = np.zeros((self.n_features, self.n_features))
-            self.S = np.zeros((self.n_features,))
+            self.S = np.zeros((self.n_features,1))
 
-        n_new,C2,S2,_x_mean2,_y_mean2 = _ScatterMats(X,y)
+        n_new = X.shape[0]
+        _x_mean2 = X.mean(axis=0)
+        X -= _x_mean2
+        _y_mean2 = y.mean(axis=0)
+        y -= _y_mean2
+
         n_old = self.n
         self.n += n_new
         f1,f2 = n_old/self.n, n_new/self.n
         # update the scatter matrices
         diff_x_mean = self._x_mean - _x_mean2
         diff_y_mean = self._y_mean - _y_mean2
-        self.C = f1*self.C + f2*C2 + f1*f2*np.outer(diff_x_mean, diff_x_mean)
-        self.S = self.S + S2 + (f1*f2*self.n)*diff_y_mean*diff_x_mean
+
+        self.S += X.T @ y
+        self.S += (f1*f2*self.n)*np.outer(diff_x_mean, diff_y_mean)
+
+        self.C += np.dot(X.T, X)
+        self.C += (f1*f2*self.n)*np.outer(diff_x_mean, diff_x_mean)
+
         # update the mean
         self._x_mean = f1*self._x_mean + f2*_x_mean2
         self._y_mean = f1*self._y_mean + f2*_y_mean2
         # computer the new project matrix
-        self.W = self._PLS1_ProjMat(self.S.copy())
+        self.W = self._PLS1_ProjMat(self.S.ravel().copy())
         return self
 
     def transform(self, X, Y=None, copy=True):
@@ -100,7 +112,7 @@ class ISIMPLS(BaseEstimator):
         self.x_rotations_ = np.dot(
             self.W,
             pinv(np.dot(self.W.T,
-                         np.dot(self.n*self.C, self.W)), check_finite=False),
+                         np.dot(self.C, self.W)), check_finite=False),
         )
 
         self.y_loadings_ = np.dot(self.S.T, self.W)
@@ -135,37 +147,36 @@ class ISIMPLS2(BaseEstimator):
     References:
 
     """
-    def __init__(self, n_components=10, copy=True):
+    def __init__(self, n_components=10, n=0, x_mean=0, y_mean=0,
+                 W=None, C=None, S=None,
+                 copy=True):
         self.__name__ = ' (ISIMPLS)'
         self.n_components = n_components
-        self.n = 0
-        self._x_mean = 0
-        self._y_mean = 0
+        self.n = n
+        self._x_mean = x_mean
+        self._y_mean = y_mean
         self.copy = copy
-        self.W = None
-        self.C = None
-        self.S = None
-        self.tic_svd = TicToc()
-        self.tic_upd = TicToc()
-        self.tim_svd = 0
-        self.tim_upd = 0
+        self.W = W
+        self.C = C
+        self.S = S
+
+    def __reduce__(self):
+        return (self.__class__, (self.n_components,
+                                 self.n, self._x_mean, self._y_mean,
+                                 self.W, self.C, self.S))
 
     def _PLS2_ProjMat(self,S):
         W_rotations = np.zeros((self.n_components,self.n_features))
         V = np.zeros((self.n_components,self.n_features))
         # compute the project matrix
-        # self.tic_svd.tic()
         wrk,_,_ = svds(S, k=1, return_singular_vectors="u")
-        # self.tim_svd += self.tic_svd.tocvalue()
 
         W_rotations[0,:] = wrk.ravel()
         p = self.C @ W_rotations[0,:]
         V[0,:] = p/norm(p)
         S -= np.outer(V[0,:], (V[0,:].T @ S))
         for k in range(1,self.n_components):
-            # self.tic_svd.tic()
             wrk,_,_ = svds(S, k= 1, return_singular_vectors="u")
-            # self.tim_svd += self.tic_svd.tocvalue()
             W_rotations[k,:] = wrk.ravel()
             p = self.C @ W_rotations[k,:]
             V[k,:] = p - V[:k,:].T @ (V[:k,:] @ p)
@@ -175,14 +186,19 @@ class ISIMPLS2(BaseEstimator):
 
     def fit(self,X,Y):
         X = check_array(X, dtype=FLOAT_DTYPES, copy=self.copy)
-        # y = check_array(y, dtype=FLOAT_DTYPES, copy=self.copy)
+        Y = check_array(Y, dtype=FLOAT_DTYPES, copy=self.copy)
+
         if self.n==0:
             self.n_features = X.shape[1]
             self.C = np.zeros((self.n_features, self.n_features))
             self.S = np.zeros((self.n_features, self.n_features))
 
-        # self.tic_upd.tic()
-        n_new,C2,S2,_x_mean2,_y_mean2 = _ScatterMats(X,Y)
+        n_new = X.shape[0]
+        _x_mean2 = X.mean(axis=0)
+        X -= _x_mean2
+        _y_mean2 = Y.mean(axis=0)
+        Y -= _y_mean2
+
         n_old = self.n
         self.n += n_new
         f1,f2 = n_old/self.n, n_new/self.n
@@ -190,17 +206,15 @@ class ISIMPLS2(BaseEstimator):
         diff_x_mean = self._x_mean - _x_mean2
         diff_y_mean = self._y_mean - _y_mean2
 
-        self.S += S2 + (f1*f2*self.n)*np.outer(diff_x_mean, diff_y_mean)
-        # self.S += (f1*f2*self.n)*np.outer(diff_x_mean, diff_y_mean)
+        self.S += np.dot(X.T, Y)
+        self.S += (f1*f2*self.n)*np.outer(diff_x_mean, diff_y_mean)
 
-        self.C *= f1
-        self.C += f2*C2 + f1*f2*np.outer(diff_x_mean, diff_x_mean)
-        # self.C += f1*f2*np.outer(diff_x_mean, diff_x_mean)
+        self.C += np.dot(X.T, X)
+        self.C += (f1*f2*self.n)*np.outer(diff_x_mean, diff_x_mean)
 
         # update the mean
         self._x_mean = f1*self._x_mean + f2*_x_mean2
         self._y_mean = f1*self._y_mean + f2*_y_mean2
-        # self.tim_upd += self.tic_upd.tocvalue()
         # computer the new project matrix
         self.W = self._PLS2_ProjMat(self.S.copy())
         return self
@@ -220,7 +234,7 @@ class ISIMPLS2(BaseEstimator):
         self.x_rotations_ = np.dot(
             W,
             pinv(np.dot(W.T,
-                         np.dot(self.n*self.C, W)), check_finite=False),
+                         np.dot(self.C, W)), check_finite=False),
         )
 
         self.y_loadings_ = np.dot(self.S.T, W)
